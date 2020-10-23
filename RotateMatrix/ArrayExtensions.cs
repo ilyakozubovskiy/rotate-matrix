2a545d91ef344808ddb58596891a782672dc90e2
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-10-23 13:37:08 +0000

    Upload New File

---
+++ RotateMatrix/ArrayExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+#pragma warning disable CA1814
+
+namespace RotateMatrix
+{
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Rotates the image clockwise by 90° and do it in place.
+        /// </summary>
+        /// <param name="matrix">Two-dimension square matrix, that presents an image.</param>
+        /// <exception cref="ArgumentNullException">Throw when source matrix is null.</exception>
+        public static void Rotate90DegreesClockwise(this int[,] matrix)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Rotates the image counterclockwise by 90° and do it in place.
+        /// </summary>
+        /// <param name="matrix">Two-dimension square matrix, that presents an image.</param>
+        /// <exception cref="ArgumentNullException">Throw when source matrix is null.</exception>
+        public static void Rotate90DegreesCounterClockwise(this int[,] matrix)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Rotates the image clockwise by 180° and do it in place.
+        /// </summary>
+        /// <param name="matrix">Two-dimension square matrix, that presents an image.</param>
+        /// <exception cref="ArgumentNullException">Throw when source matrix is null.</exception>
+        public static void Rotate180DegreesClockwise(this int[,] matrix)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Rotates the image counterclockwise by 180° and do it in place.
+        /// </summary>
+        /// <param name="matrix">Two-dimension square matrix, that presents an image.</param>
+        /// <exception cref="ArgumentNullException">Throw when source matrix is null.</exception>
+        public static void Rotate180DegreesCounterClockwise(this int[,] matrix)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Rotates the image clockwise by 270° and do it in place.
+        /// </summary>
+        /// <param name="matrix">Two-dimension square matrix, that presents an image.</param>
+        /// <exception cref="ArgumentNullException">Throw when source matrix is null.</exception>
+        public static void Rotate270DegreesClockwise(this int[,] matrix)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Rotates the image counterclockwise by 270° and do it in place.
+        /// </summary>
+        /// <param name="matrix">Two-dimension square matrix, that presents an image.</param>
+        /// <exception cref="ArgumentNullException">Throw when source matrix is null.</exception>
+        public static void Rotate270DegreesCounterClockwise(this int[,] matrix)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Rotates the image clockwise by 360° and do it in place.
+        /// </summary>
+        /// <param name="matrix">Two-dimension square matrix, that presents an image.</param>
+        /// <exception cref="ArgumentNullException">Throw when source matrix is null.</exception>
+        public static void Rotate360DegreesClockwise(this int[,] matrix)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Rotates the image counterclockwise by 360° and do it in place.
+        /// </summary>
+        /// <param name="matrix">Two-dimension square matrix, that presents an image.</param>
+        /// <exception cref="ArgumentNullException">Throw when source matrix is null.</exception>
+        public static void Rotate360DegreesCounterClockwise(this int[,] matrix)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
